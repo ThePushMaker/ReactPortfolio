e7dc5837469ddaf3bd972b63dcbbce216f9087c1
DSW <elmcwl@gmail.com>
2023-04-20 11:04:02 -0700

    hero section

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
+import { Skills } from './components/Skills'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-        <NavBar/>
-        <Banner/>
+        <NavBar />
+        <Banner />
+        <Skills />
     </div>
   );
 }
