f4d9ca06c8c2f73b5bfd2fa2cc1122b3f674ebc5
DSW <elmcwl@gmail.com>
2023-04-20 22:51:14 -0700

    contact section done

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
-import { Skills } from './components/Skills'
-import { Projects } from './components/Projects'
+import { Skills } from './components/Skills';
+import { Projects } from './components/Projects';
+import { Contact } from './components/Contact';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Banner />
         <Skills />
         <Projects />
+        <Contact />
     </div>
   );
 }
