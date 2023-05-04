85c812e21886d3687b6390ab9050881ad52708b6
DSW <elmcwl@gmail.com>
2023-05-04 01:52:39 -0700

    fix contacto envio de correo

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,48 +1,48 @@
 import { useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
-import contactImg from '../assets/img/contact-img.svg'
+import { Container, Row, Col } from "react-bootstrap";
+import contactImg from "../assets/img/contact-img.svg";
 import 'animate.css';
-import TrackVisibility from "react-on-screen";
+import TrackVisibility from 'react-on-screen';
 
 export const Contact = () => {
-    const formInitialDetails = {
-        firstName: '',
-        lastName: '',
-        email: '',
-        phone: '',
-        message: ''
+  const formInitialDetails = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    message: ''
+  }
+  const [formDetails, setFormDetails] = useState(formInitialDetails);
+  const [buttonText, setButtonText] = useState('Send');
+  const [status, setStatus] = useState({});
+
+  const onFormUpdate = (category, value) => {
+      setFormDetails({
+        ...formDetails,
+        [category]: value
+      })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setButtonText("Sending...");
+    let response = await fetch("http://localhost:5000/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(formDetails),
+    });
+    setButtonText("Send");
+    let result = await response.json();
+    setFormDetails(formInitialDetails);
+    if (result.code == 200) {
+      setStatus({ succes: true, message: 'Message sent successfully'});
+    } else {
+      setStatus({ succes: false, message: 'Something went wrong, please try again later.'});
     }
+  };
 
-    const [ formDetails, setFormDetails ] = useState(formInitialDetails);
-    const [ buttonText, setButtonText ] = useState('Send');
-    const [ status, setStatus ] = useState({});
-
-    const onFormUpdate= (category, value) => {
-        setFormDetails({
-            ...formDetails,
-            [category]: value
-        })
-    }
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setButtonText("Sending...");
-        let response = await fetch("http://localhost:5000/contact", {
-            method: "POST",
-            headers: {
-                "Content-Type": "Application/json;charset=utf-8"
-            },
-            body: JSON.stringify(formDetails),
-        });
-        setButtonText("Send");
-        let result = response.json();
-        setFormDetails(formInitialDetails);
-        if (result.code == 200) {
-            setStatus({ success: true, message: 'Message sent successfully.'});
-        }else {
-            setStatus({ success: false, message: 'Something went wrong, please try again later.'});
-        }
-    };
 
     return (
         <section className="contact" id ="connect">
